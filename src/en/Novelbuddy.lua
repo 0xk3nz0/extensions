6bdb350bc6d7696a4d085959f36a00b34d677012
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2026-04-21 14:47:04 +0000

    fix: Update Novelbuddy due to website update

--- src/en/Novelbuddy.lua
+++ src/en/Novelbuddy.lua
@@ -1,6 +1,9 @@
--- {"id":95566,"ver":"1.0.2","libVer":"1.0.0","author":"Confident-hate"}
+-- {"id":95566,"ver":"2.0.0","libVer":"1.0.0","author":"Confident-hate","dep":["dkjson>=1.0.0, unhtml>=1.0.0"]}
 
 local baseURL = "https://novelbuddy.com"
+local apiURL = "https://api.novelbuddy.com"
+local json = Require("dkjson")
+local unhtml = Require("unhtml")
 
 ---@param v Element
 local text = function(v)
@@ -156,12 +159,25 @@ local searchFilters = {
     DropdownFilter(STATUS_FILTER, "Status", STATUS_VALUES)
 }
 
+
+
+local headers = HeadersBuilder():add("Accept", "text/css"):build()
+
+--- takes a url, returns table formatted json
+--- @param url string
+--- @return string
+local function jsonGET(url)
+    local res = Request(GET(url, headers))
+    return json.decode(res:body():string())
+end
+
 --- @param chapterURL string @url of the chapter
 --- @return string @of chapter
 local function getPassage(chapterURL)
     local htmlElement = GETDocument(chapterURL)
-    local chapter = htmlElement:selectFirst(".content-inner")
-    local title = htmlElement:selectFirst("h1"):text()
+    local reader = htmlElement:selectFirst(".novel-reader-content")
+    local title = reader:selectFirst("h3"):text()
+    local chapter = reader:children():get(1) -- 0 is title banner
     chapter:prepend("<h1>" .. title .. "</h1>")
 
     -- stolen Code from novelvault, see comment line 550
@@ -175,6 +191,9 @@ local function getPassage(chapterURL)
             if previous and next and previous:tagName() == "p" and next:tagName() == "p" then
                 table.insert(toRemove, v)
             end
+        
+        elseif tag == "div" and v:hasClass("my-4") then
+            table.insert(toRemove, v)
         end
     end, function() end, true)) -- Enable elements only to avoid crashes
 
@@ -182,19 +201,19 @@ local function getPassage(chapterURL)
         node:remove()
     end
 
-    return pageOfElem(chapter, false)
+    return pageOfElem(chapter, true)
 end
 
 --- @param data table
 local function search(data)
     local queryContent = data[QUERY]
     local page = "&page=" .. data[PAGE]
-    local document = GETDocument(baseURL .. "/search?q=" .. queryContent .. page)
-    return map(document:select(".section-body .book-item"), function(v)
+    local searchData = jsonGET(apiURL .. "/titles/search?q=" .. queryContent .. page).data
+    return map(searchData.items, function(v)
         return Novel {
-            title = v:selectFirst(".book-detailed-item .title h3 a"):text(),
-            imageURL = "http:" .. v:selectFirst(".book-detailed-item .thumb img"):attr("data-src"),
-            link = v:selectFirst(".book-detailed-item .title h3 a"):attr("href")
+            title = v.name,
+            imageURL = v.cover,
+            link = v.url
         }
     end)
 end
@@ -204,41 +223,52 @@ end
 local function parseNovel(novelURL)
     local url = baseURL .. novelURL
     local document = GETDocument(url)
-    local novelID = string.match(document:selectFirst(".layout > script"):html(), ".*bookId = (%d*);")
-    local chapterURL = "https://novelbuddy.com/api/manga/" .. novelID .. "/chapters?source=detail"
-    local chaperDoc = GETDocument(chapterURL)
-    local chapterOrder = chaperDoc:select(".chapter-list li a"):size()
+    local jsonData = json.decode(document:getElementById("__NEXT_DATA__"):html())
+    local novelData = jsonData.props.pageProps.initialManga
+    local novelID = novelData.id
+    local chapterURL = apiURL .. "/titles/" .. novelID .. "/chapters"
+    local chapterList = jsonGET(chapterURL).data.chapters
+    local chapterOrder = #chapterList
     return NovelInfo {
-        title = document:selectFirst(".detail .name.box h1"):text(),
-        description = document:selectFirst(".section-body.summary .content"):text(),
-        imageURL = "http:" .. document:selectFirst(".img-cover img"):attr("data-src"),
+        title = novelData.name,
+        -- alternativeTitles = novelData.altName or "", -- Seems to be broken
+        -- description = unhtml:HTMLToString("<div>"..novelData.summary.."</div>"),
+        description = novelData.summary,
+        imageURL = novelData.cover,
         status = ({
             Ongoing = NovelStatus.PUBLISHING,
+            OnGoing = NovelStatus.PUBLISHING,
             Completed = NovelStatus.COMPLETED,
-        })[document:selectFirst("div.meta:nth-child(2) > p:nth-child(2) > a:nth-child(2) > span:nth-child(1)"):text()],
-        authors = map(document:selectFirst("div.meta:nth-child(2) > p:nth-child(1)"):select("a span"), text ),
-        genres = map(document:selectFirst("div.meta:nth-child(2) > p:nth-child(3)"):select("a"), text ),
-        tags = map(document:select(".tags a"), text ),
+        })[novelData.status] or NovelStatus.UNKNOWN,
+        authors = map(novelData.authors, function (v) return v.name end),
+        genres = map(novelData.genres, function (v) return v.name end),
+        tags = map(novelData.tags, function (v) return v.name end),
         chapters = AsList(
-            map(chaperDoc:select(".chapter-list li a"), function(v)
+            map(chapterList, function(v)
                 chapterOrder = chapterOrder - 1
                 return NovelChapter {
                     order = chapterOrder,
-                    title = v:selectFirst(".chapter-title"):text(),
-                    link = baseURL .. v:attr("href")
+                    title = v.name,
+                    link = baseURL .. v.url,
+                    release = v.updated_at:sub(1,10), -- substr match YYYY-MM-DD out of ISO datetime
+                    --- sourceId = v.id -- Novel.kt's Chapter class at kotlin-lib only takes Int id
                 }
             end)
         )
     }
 end
 
+
+-- Weirdly not served in json like everything else. At least I couldn't find
+-- it by looking at the network tab in browser's inspect window.
+-- You'd think if search is json, this would be too.
 local function parseListing(listingURL)
     local document = GETDocument(listingURL)
-    return map(document:select(".section-body .book-item"), function(v)
+    return map(document:select(".object-cover"), function(v)
         return Novel {
-            title = v:selectFirst(".book-detailed-item .title h3 a"):text(),
-            imageURL = "http:" .. v:selectFirst(".book-detailed-item .thumb img"):attr("data-src"),
-            link = v:selectFirst(".book-detailed-item .title h3 a"):attr("href")
+            title = v:attr("title"),
+            imageURL = v:attr("src"),
+            link = v:parent():attr("href")
         }
     end)
 end
@@ -278,9 +308,7 @@ return {
     listings = {
         getListing("Popular", true, "/popular"),
         getListing("Newest", true, "/newest"),
-        getListing("Chinese", true, "/types/chinese"),
-        getListing("Korean", true, "/types/korean"),
-        getListing("Japanese", true, "/types/japanese")
+        getListing("Latest", true, "/latest")
     },
     parseNovel = parseNovel,
     getPassage = getPassage,
@@ -290,3 +318,4 @@ return {
     expandURL = expandURL,
     searchFilters = searchFilters
 }
+
